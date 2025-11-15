34688644cd8c98329049e4d557bd4ed5c7061357
GulnazNurseit <gulnaznurseit1227@gmail.com>
2025-11-15 18:04:27 +0500

    Update PaymentAdapter.java

--- PaymentAdapter.java
+++ PaymentAdapter.java
@@ -1,12 +1,22 @@
 public class PaymentAdapter implements PaymentProcessor {
     private ExternalPaymentSystem external;
-
     public PaymentAdapter(ExternalPaymentSystem external) {
         this.external = external;
     }
-
     @Override
-    public void processPayment(double amount, int method) {
-        external.makePayment(amount);
+    public void processPayment(double amount, int method, String cardNumber, String expiry, String cvv) {
+        switch (method) {
+            case 1: // Credit Card
+                external.processCreditCard(amount, cardNumber, expiry, cvv);
+                break;
+            case 2: // PayPal
+                external.processPayPal(amount);
+                break;
+            case 3: // Cash
+                external.processCash(amount);
+                break;
+            default:
+                System.out.println("❌ Unknown payment method");
+        }
     }
 }
